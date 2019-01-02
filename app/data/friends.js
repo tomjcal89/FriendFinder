aef6384b6fc830639e24348744c7b87aa1877dfe
Thomas J Calhoun <thomascalhoun@Thomass-MBP.attlocal.net>
2019-01-01 21:31:15 -0800

    finsihed

---
+++ app/data/friends.js
@@ -0,0 +1,70 @@
+let friend = [
+    
+    {
+        "name": "Justin Timberlake",
+        "photo": "https://nyppagesix.files.wordpress.com/2018/02/gettyimages-629251016.jpg?quality=90&strip=all&w=642",
+        "scores": [
+            "4",
+            "4",
+            "5",
+            "3",
+            "3",
+            "2",
+            "3",
+            "1",
+            "3",
+            "2"
+        ]
+    },
+    {
+        "name": "Lady Gaga",
+        "photo": "https://www.latimes.com/resizer/VsH-8lzZMk8i7FFJlU9zly20MW4=/800x0/www.trbimg.com/img-5b84d2da/turbine/la-1535431382-2gsj8kys3x-snap-image",
+        "scores": [
+            "5",
+            "2",
+            "2",
+            "2",
+            "4",
+            "1",
+            "1",
+            "1",
+            "5",
+            "5"
+        ]
+    },
+    
+    {
+        "name": "Jennifer Aniston",
+        "photo": "https://cdn-img.instyle.com/sites/default/files/styles/684xflex/public/1532533939/jennifer-aniston-001.jpg?itok=-sE8e0Is",
+        "scores": [
+            "4",
+            "3",
+            "4",
+            "1",
+            "5",
+            "2",
+            "5",
+            "3",
+            "1",
+            "4"
+        ]
+    },
+    {
+        "name": "Oprah",
+        "photo": "https://media.newyorker.com/photos/5a539aaab0bc43580507ac4a/master/w_813,c_limit/St-Felix-Oprah-2020.jpg",
+        "scores": [
+            "4",
+            "4",
+            "2",
+            "3",
+            "2",
+            "1",
+            "4",
+            "5",
+            "4",
+            "1"
+        ]
+    }
+]
+
+module.exports = friend
